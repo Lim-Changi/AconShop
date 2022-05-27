afb96575b5556d6cb72ecff1842f96039cbd9cc4
changi <limchangi@naver.com>
2022-05-27 22:22:22 +0900

    [Feat]: ConfigModule 전역변수 joi Validation 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { HealthCheckController } from './HealthCheck/HealthCheckController';
 import { TerminusModule } from '@nestjs/terminus';
+import { ValidationSchema } from '@app/common/config/validationSchema';
 
 @Module({
-  imports: [HttpModule, TerminusModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: ValidationSchema,
+    }),
+    HttpModule,
+    TerminusModule,
+  ],
   controllers: [HealthCheckController],
 })
 export class AppModule {}

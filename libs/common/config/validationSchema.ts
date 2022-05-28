542ba89379e48ac876baab99cda15ee9f963d076
찬기 <69755603+Lim-Changi@users.noreply.github.com>
2022-05-28 15:48:32 +0900

    Revert "[chore]: NestJS Project Base Setup"

--- libs/common/config/validationSchema.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from 'joi';
-
-export const ValidationSchema = Joi.object({
-  NODE_ENV: Joi.string().required(),
-  PORT: Joi.string().required(),
-  SWAGGER_ADMIN: Joi.string().required(),
-  SWAGGER_PASSWORD: Joi.string().required(),
-});
